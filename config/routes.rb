a471dc62f5f845fb4bcbdc1a3c446be68c557dd7
vagrant <vagrant@ubuntu1804.localdomain>
2019-09-16 14:11:49 -0500

    About page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/profile', status: 302)
+  get 'profile', to: 'pages#profile', as: 'profile'
 end
