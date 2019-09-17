bc939c25401bfc47c69647f14bd7e9d545b6b2cb
vagrant <vagrant@ubuntu1804.localdomain>
2019-09-17 14:15:18 -0500

    Question page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,23 @@
 class PagesController < ApplicationController
-    def about
-        respond_to do |format|
-          format.html { render :about }
-        end
-      end
-      def review
-        respond_to do |format|
-          format.html { render :review }
-        end
-      end
+  def about
+    respond_to do |format|
+      format.html { render :about }
     end
+  end
+  def review
+    respond_to do |format|
+      format.html { render :review }
+    end
+  end
+  def question
+    answers = [
+      "a) A class should have only one reason to change.",
+      "b) Each developer on a team should have one main responsibility.",
+      "c) Every app will have one responsibility that is most important to users."
+    ]
+    respond_to do |format|
+      format.html { render :question, locals: { answers: answers }  }
+    end
+  end
+end
+    
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: redirect('/drllano1', status: 302)
   get 'drllano1', to: 'pages#profile', as: 'drllano1'
   get 'review', to: 'pages#review', as: 'review'
+  get 'question', to: 'pages#question', as: 'question'
 end
